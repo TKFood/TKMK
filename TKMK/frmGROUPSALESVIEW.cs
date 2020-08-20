58aa3a343c6344ae113a0f30afcd4050a29a0be1
at3158 <tk290@tkfood.com.tw>
2020-08-20 09:39:07 +0800

    frmGROUPSALESVIEW

--- TKMK/frmGROUPSALESVIEW.cs
+++ TKMK/frmGROUPSALESVIEW.cs
@@ -133,11 +133,12 @@ namespace TKMK
                 sbSqlQuery.Clear();
 
                 sbSql.AppendFormat(@"  SELECT ");
-                sbSql.AppendFormat(@"  [SERNO] AS '序號',[CARNO] AS '車號',[CARNAME] AS '車名',[CARKIND] AS '車種',[GROUPKIND]  AS '團類',[ISEXCHANGE] AS '兌換券',[EXCHANGETOTALMONEYS] AS '券總額',[EXCHANGESALESMMONEYS] AS '券消費',[SALESMMONEYS] AS '消費總額'");
+                sbSql.AppendFormat(@"  [SERNO] AS '序號',[CARNAME] AS '車名',[CARNO] AS '車號',[CARKIND] AS '車種',[GROUPKIND]  AS '團類',[ISEXCHANGE] AS '兌換券',[EXCHANGETOTALMONEYS] AS '券總額',[EXCHANGESALESMMONEYS] AS '券消費',[SALESMMONEYS] AS '消費總額'");
                 sbSql.AppendFormat(@"  ,[SPECIALMNUMS] AS '特賣數',[SPECIALMONEYS] AS '特賣獎金',[COMMISSIONBASEMONEYS] AS '茶水費',[COMMISSIONPCTMONEYS] AS '消費獎金',[TOTALCOMMISSIONMONEYS] AS '總獎金',[CARNUM] AS '車數',[GUSETNUM] AS '來客數',[EXCHANNO] AS '優惠券名',[EXCHANACOOUNT] AS '優惠券帳號',CONVERT(varchar(100), [GROUPSTARTDATES],120) AS '實際到達時間',CONVERT(varchar(100), [GROUPENDDATES],120) AS '實際離開時間',[STATUS] AS '狀態'");
                 sbSql.AppendFormat(@"  ,CONVERT(varchar(100), [PURGROUPSTARTDATES],120) AS '預計到達時間',CONVERT(varchar(100), [PURGROUPENDDATES],120) AS '預計離開時間',[COMMISSIONPCT] AS '抽佣比率',[EXCHANGEMONEYS] AS '領券額',[ID],[CREATEDATES]");
                 sbSql.AppendFormat(@"  FROM [TKMK].[dbo].[GROUPSALES]");
                 sbSql.AppendFormat(@"  WHERE CONVERT(nvarchar,[CREATEDATES],112)='{0}' ", CREATEDATES);
+                sbSql.AppendFormat(@"  AND [STATUS] NOT IN ('取消預約') ");
                 sbSql.AppendFormat(@"  ORDER BY CONVERT(nvarchar,[CREATEDATES],112),CONVERT(int,[SERNO]) DESC");
                 sbSql.AppendFormat(@"  ");
                 sbSql.AppendFormat(@"  ");
@@ -167,7 +168,7 @@ namespace TKMK
                         dataGridView1.DefaultCellStyle.Font = new Font("Tahoma", 10);
                         dataGridView1.Columns[0].Width = 30;
                         dataGridView1.Columns[1].Width = 80;
-                        dataGridView1.Columns[2].Width = 60;
+                        dataGridView1.Columns[2].Width = 80;
                         dataGridView1.Columns[3].Width = 40;
                         dataGridView1.Columns[4].Width = 80;
                         dataGridView1.Columns[5].Width = 20;
@@ -223,6 +224,9 @@ namespace TKMK
                 if (ROWSINDEX > 0 || COLUMNSINDEX > 0)
                 {
                     dataGridView1.CurrentCell = dataGridView1.Rows[ROWSINDEX].Cells[COLUMNSINDEX];
+
+                    DataGridViewRow row = dataGridView1.Rows[ROWSINDEX];
+                    
 
                 }
             }
