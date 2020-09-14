e6a7cdf27c01e40a995439a50477f74085f0d185
at3158 <tk290@tkfood.com.tw>
2020-09-14 14:56:42 +0800

    SEARCHGROUPSALES

--- TKMK/frmGROUPSALES.cs
+++ TKMK/frmGROUPSALES.cs
@@ -309,17 +309,15 @@ namespace TKMK
                 sbSql.Clear();
                 sbSqlQuery.Clear();
 
-                sbSql.AppendFormat(@"  SELECT ");
-                sbSql.AppendFormat(@"  [SERNO] AS '序號',[CARNAME] AS '車名',[CARNO] AS '車號',[CARKIND] AS '車種',[GROUPKIND]  AS '團類',[ISEXCHANGE] AS '兌換券',[EXCHANGETOTALMONEYS] AS '券總額',[EXCHANGESALESMMONEYS] AS '券消費',[SALESMMONEYS] AS '消費總額'");
-                sbSql.AppendFormat(@"  ,[SPECIALMNUMS] AS '特賣數',[SPECIALMONEYS] AS '特賣獎金',[COMMISSIONBASEMONEYS] AS '茶水費',[COMMISSIONPCTMONEYS] AS '消費獎金',[TOTALCOMMISSIONMONEYS] AS '總獎金',[CARNUM] AS '車數',[GUSETNUM] AS '來客數',[EXCHANNO] AS '優惠券名',[EXCHANACOOUNT] AS '優惠券帳號',CONVERT(varchar(100), [GROUPSTARTDATES],120) AS '實際到達時間',CONVERT(varchar(100), [GROUPENDDATES],120) AS '實際離開時間',[STATUS] AS '狀態'");
-                sbSql.AppendFormat(@"  ,CONVERT(varchar(100), [PURGROUPSTARTDATES],120) AS '預計到達時間',CONVERT(varchar(100), [PURGROUPENDDATES],120) AS '預計離開時間',[COMMISSIONPCT] AS '抽佣比率',[EXCHANGEMONEYS] AS '領券額',[ID],[CREATEDATES]");
-                sbSql.AppendFormat(@"  FROM [TKMK].[dbo].[GROUPSALES]");
-                sbSql.AppendFormat(@"  WHERE CONVERT(nvarchar,[CREATEDATES],112)='{0}' ", CREATEDATES);
-                sbSql.AppendFormat(@"  AND [STATUS]<>'取消預約'");
-                sbSql.AppendFormat(@"  ORDER BY CONVERT(nvarchar,[CREATEDATES],112),CONVERT(int,[SERNO]) DESC");
-                sbSql.AppendFormat(@"  ");
-                sbSql.AppendFormat(@"  ");
-                sbSql.AppendFormat(@"  ");
+                sbSql.AppendFormat(@" SELECT 
+                                    [SERNO] AS '序號',[CARNAME] AS '車名',[CARNO] AS '車號',[CARKIND] AS '車種',[GROUPKIND]  AS '團類',[ISEXCHANGE] AS '兌換券',[EXCHANGETOTALMONEYS] AS '券總額',[EXCHANGESALESMMONEYS] AS '券消費',[SALESMMONEYS] AS '消費總額'
+                                    ,[SPECIALMNUMS] AS '特賣數',[SPECIALMONEYS] AS '特賣獎金',[COMMISSIONBASEMONEYS] AS '茶水費',[COMMISSIONPCTMONEYS] AS '消費獎金',[TOTALCOMMISSIONMONEYS] AS '總獎金',[CARNUM] AS '車數',[GUSETNUM] AS '來客數',[EXCHANNO] AS '優惠券名',[EXCHANACOOUNT] AS '優惠券帳號',CONVERT(varchar(100), [GROUPSTARTDATES],120) AS '實際到達時間',CONVERT(varchar(100), [GROUPENDDATES],120) AS '實際離開時間',[STATUS] AS '狀態'
+                                    ,CONVERT(varchar(100), [PURGROUPSTARTDATES],120) AS '預計到達時間',CONVERT(varchar(100), [PURGROUPENDDATES],120) AS '預計離開時間',[COMMISSIONPCT] AS '抽佣比率',[EXCHANGEMONEYS] AS '領券額',[ID],[CREATEDATES]
+                                    FROM [TKMK].[dbo].[GROUPSALES]
+                                    WHERE CONVERT(nvarchar,[CREATEDATES],112)='20200914'
+                                    AND [STATUS]<>'取消預約'
+                                    ORDER BY CONVERT(nvarchar,[CREATEDATES],112),CONVERT(int,[SERNO]) DESC ", CREATEDATES);
+
 
                 adapter1 = new SqlDataAdapter(@"" + sbSql, sqlConn);
 
@@ -345,7 +343,7 @@ namespace TKMK
                         dataGridView1.DefaultCellStyle.Font = new Font("Tahoma", 10);
                         dataGridView1.Columns[0].Width = 30;
                         dataGridView1.Columns[1].Width = 80;
-                        dataGridView1.Columns[2].Width = 80;
+                        dataGridView1.Columns["車號"].Width = 100;
                         dataGridView1.Columns[3].Width = 40;
                         dataGridView1.Columns[4].Width = 80;
                         dataGridView1.Columns[5].Width = 20;
@@ -376,6 +374,16 @@ namespace TKMK
                         //根据列表中数据不同，显示不同颜色背景
                         foreach (DataGridViewRow dgRow in dataGridView1.Rows)
                         {
+                            dgRow.Cells["車號"].Style.Font = new Font("Tahoma", 14);
+                            dgRow.Cells["券總額"].Style.Font= new Font("Tahoma", 14);
+                            dgRow.Cells["券消費"].Style.Font = new Font("Tahoma", 14);
+                            dgRow.Cells["消費總額"].Style.Font = new Font("Tahoma", 14);
+                            dgRow.Cells["特賣數"].Style.Font = new Font("Tahoma", 14);
+                            dgRow.Cells["特賣獎金"].Style.Font = new Font("Tahoma", 14);
+                            dgRow.Cells["茶水費"].Style.Font = new Font("Tahoma", 14);
+                            dgRow.Cells["總獎金"].Style.Font = new Font("Tahoma", 14);
+                            dgRow.Cells["來客數"].Style.Font = new Font("Tahoma", 14);
+
                             //判断
                             if (dgRow.Cells[20].Value.ToString().Trim().Equals("完成接團"))
                             {
