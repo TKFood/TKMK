6ea0f947b1fd5e5a2061d4873f64f6b09f22201a
at3158 <tk290@tkfood.com.tw>
2021-10-14 15:19:49 +0800

    SETSQL3

--- TKMK/frmREPORTFACTORYINCOME.cs
+++ TKMK/frmREPORTFACTORYINCOME.cs
@@ -23,6 +23,7 @@ using NPOI.XSSF.UserModel;
 using FastReport;
 using FastReport.Data;
 using TKITDLL;
+using System.Globalization;
 
 namespace TKMK
 {
@@ -208,6 +209,125 @@ namespace TKMK
             return SB;
 
         }
+
+        public void SETFASTREPORT2(DateTime SDATES)
+        {
+            StringBuilder SQL2 = new StringBuilder();
+            StringBuilder SQL3 = new StringBuilder();
+
+
+            string YEARS = SDATES.ToString("yyyy");
+            string LASTYEARS = SDATES.AddYears(-1).ToString("yyyy");
+
+            string INDATES = SDATES.ToString("yyyyMM");
+            string LASTINDATES = SDATES.AddYears(-1).ToString("yyyyMM");
+
+            GregorianCalendar gc = new GregorianCalendar();
+            int WEEKOFYEARS = gc.GetWeekOfYear(SDATES, CalendarWeekRule.FirstDay, DayOfWeek.Monday);
+            string WEEKS1 = (WEEKOFYEARS - 1).ToString();
+            string WEEKS2 = (WEEKOFYEARS - 2).ToString();
+            string WEEKS3 = (WEEKOFYEARS - 3).ToString();
+            string WEEKS4 = (WEEKOFYEARS - 4).ToString();
+
+
+
+            SQL2 = SETSQL2(YEARS, LASTYEARS, INDATES, LASTINDATES);
+            SQL3 = SETSQL3(YEARS, LASTYEARS, WEEKS1, WEEKS2, WEEKS3, WEEKS4);
+
+            Report report1 = new Report();
+            report1.Load(@"REPORT\觀光業績及車次比較表.frx");
+
+            //20210902密
+            Class1 TKID = new Class1();//用new 建立類別實體
+            SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+            //資料庫使用者密碼解密
+            sqlsb.Password = TKID.Decryption(sqlsb.Password);
+            sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+            String connectionString;
+            sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+            report1.Dictionary.Connections[0].ConnectionString = sqlsb.ConnectionString;
+
+
+            TableDataSource table = report1.GetDataSource("Table") as TableDataSource;
+            table.SelectCommand = SQL2.ToString();
+
+            TableDataSource table1 = report1.GetDataSource("Table1") as TableDataSource;
+            table1.SelectCommand = SQL3.ToString();
+
+            //report1.SetParameterValue("P1", dateTimePicker1.Value.ToString("yyyyMMdd"));
+            //report1.SetParameterValue("P2", dateTimePicker2.Value.ToString("yyyyMMdd"));
+            report1.Preview = previewControl1;
+            report1.Show();
+        }
+
+        public StringBuilder SETSQL2(string YEARS, string LASTYEARS,string INDATES,string LASTINDATES)
+        {
+            StringBuilder SB = new StringBuilder();
+
+
+            SB.AppendFormat(@" 
+                            SELECT [YEARS] AS '年月',SUM([CARNUM]) AS '來車數' 
+                            FROM [TKMK].[dbo].[TBFACTORYINCOME]
+                            WHERE [YEARS]='{0}'
+                            GROUP BY [YEARS]
+                            UNION ALL
+                            SELECT [YEARS],SUM([CARNUM]) AS 'CARNUM'
+                            FROM [TKMK].[dbo].[TBFACTORYINCOME]
+                            WHERE [YEARS]='{1}'
+                            GROUP BY [YEARS]
+                            UNION ALL
+                            SELECT SUBSTRING([INDATES],1,6),SUM([CARNUM]) AS 'CARNUM'
+                            FROM [TKMK].[dbo].[TBFACTORYINCOME]
+                            WHERE[INDATES] LIKE '{2}%'
+                            GROUP BY SUBSTRING([INDATES],1,6)
+                            UNION ALL
+                            SELECT SUBSTRING([INDATES],1,6),SUM([CARNUM]) AS 'CARNUM'
+                            FROM [TKMK].[dbo].[TBFACTORYINCOME]
+                            WHERE [INDATES] LIKE '{3}%'
+                            GROUP BY SUBSTRING([INDATES],1,6)
+ 
+
+                            ", YEARS, LASTYEARS, INDATES, LASTINDATES);
+
+            return SB;
+
+        }
+
+        public StringBuilder SETSQL3(string YEARS,string LASTYEARS,string WEEKS1, string WEEKS2, string WEEKS3, string WEEKS4)
+        {
+            StringBuilder SB = new StringBuilder();
+
+
+            SB.AppendFormat(@" 
+                            SELECT 
+                            [YEARS] AS '年度'
+                            ,[WEEKS] AS '週次'
+                            ,SUM([TOTALMONEYS]) AS '銷售組業績'
+                            ,SUM([GROUPMONEYS]) AS '團客業績'
+                            ,SUM([VISITORMONEYS]) AS '散客業績'
+                            ,SUM([CARNUM]) AS '遊覽車次'
+                            ,AVG([CARAVGMONEYS]) AS '每車平均業績'
+                            ,(SELECT SUM(TOTALMONEYS) FROM [TKMK].[dbo].[TBFACTORYINCOME] LASTTBFACTORYINCOME WHERE LASTTBFACTORYINCOME.WEEKS=[TBFACTORYINCOME].WEEKS AND LASTTBFACTORYINCOME.YEARS='{1}')   AS '同期業績'
+                            ,(SELECT SUM(GROUPMONEYS) FROM [TKMK].[dbo].[TBFACTORYINCOME] LASTTBFACTORYINCOME WHERE LASTTBFACTORYINCOME.WEEKS=[TBFACTORYINCOME].WEEKS AND LASTTBFACTORYINCOME.YEARS='{1}')  AS '同期團客'
+                            ,(SELECT SUM(VISITORMONEYS) FROM [TKMK].[dbo].[TBFACTORYINCOME] LASTTBFACTORYINCOME WHERE LASTTBFACTORYINCOME.WEEKS=[TBFACTORYINCOME].WEEKS AND LASTTBFACTORYINCOME.YEARS='{1}')  AS '同期散客'
+                            ,(SELECT SUM(CARNUM) FROM [TKMK].[dbo].[TBFACTORYINCOME] LASTTBFACTORYINCOME WHERE LASTTBFACTORYINCOME.WEEKS=[TBFACTORYINCOME].WEEKS AND LASTTBFACTORYINCOME.YEARS='{1}')  AS '同期車次'
+                            ,(SELECT AVG(CARAVGMONEYS) FROM [TKMK].[dbo].[TBFACTORYINCOME] LASTTBFACTORYINCOME WHERE LASTTBFACTORYINCOME.WEEKS=[TBFACTORYINCOME].WEEKS AND LASTTBFACTORYINCOME.YEARS='{1}')  AS '同期平均業績'
+                            FROM [TKMK].[dbo].[TBFACTORYINCOME]
+                            WHERE YEARS='{0}'
+                            AND WEEKS IN ('{2}','{3}' ,'{4}' ,'{5}')
+                            GROUP BY [YEARS],[WEEKS]
+ 
+
+                            ", YEARS, LASTYEARS, WEEKS1, WEEKS2, WEEKS3, WEEKS4);
+
+            return SB;
+
+        }
+
+
         #endregion
 
         #region BUTTON
@@ -221,7 +341,7 @@ namespace TKMK
         }
         private void button3_Click(object sender, EventArgs e)
         {
-
+            SETFASTREPORT2(dateTimePicker1.Value);
         }
 
         #endregion
