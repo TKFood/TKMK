f924c96359fd34f35e6acb01f85f5e8d8d4a7327
at3158 <tk290@tkfood.com.tw>
2023-11-29 11:58:11 +0800

    addfrmREPORTSTORESQUESTIONAIRES

---
+++ TKMK/frmREPORTSTORESQUESTIONAIRES.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TKMK
+{
+    public partial class frmREPORTSTORESQUESTIONAIRES : Form
+    {
+        public frmREPORTSTORESQUESTIONAIRES()
+        {
+            InitializeComponent();
+        }
+    }
+}
