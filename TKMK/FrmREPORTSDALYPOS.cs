d2534ce15c92ed4d58d12d92855c996215627d90
at3158 <tk290@tkfood.com.tw>
2025-03-26 14:09:09 +0800

    ADD_TBDAILYPOSTB

--- TKMK/FrmREPORTSDALYPOS.cs
+++ TKMK/FrmREPORTSDALYPOS.cs
@@ -204,12 +204,172 @@ namespace TKMK
 
         }
 
+        public void ADD_TBDAILYPOSTB(string SDATES)
+        {
+            StringBuilder sbSql = new StringBuilder();
+            SqlTransaction tran;
+            SqlCommand cmd = new SqlCommand();
+            int result;
+            try
+            {
+                //20210902密
+                Class1 TKID = new Class1();//用new 建立類別實體
+                SqlConnectionStringBuilder sqlsb = new SqlConnectionStringBuilder(ConfigurationManager.ConnectionStrings["dbconn"].ConnectionString);
+
+                //資料庫使用者密碼解密
+                sqlsb.Password = TKID.Decryption(sqlsb.Password);
+                sqlsb.UserID = TKID.Decryption(sqlsb.UserID);
+
+                String connectionString;
+                sqlConn = new SqlConnection(sqlsb.ConnectionString);
+
+
+                sbSql.Clear();
+                sqlConn.Close();
+                sqlConn.Open();
+                tran = sqlConn.BeginTransaction();
+
+
+                sbSql.AppendFormat(@" 
+                                    DELETE [TKMK].[dbo].[TBDAILYPOSTB]
+                                    WHERE [SDATES]='{0}'
+
+                                    --新增日期+品號
+                                    INSERT INTO [TKMK].[dbo].[TBDAILYPOSTB]
+                                    ([SDATES]
+                                    ,[MB001]
+                                    ,[MB002]
+                                    )
+                                    SELECT DISTINCT '{0}' , MB001, MB002
+                                    FROM 
+                                    (
+                                        SELECT LA001 AS MB001, MB002 AS MB002
+                                        FROM [TK].dbo.INVLA
+                                        INNER JOIN [TK].dbo.INVMB ON LA001 = MB001
+                                        WHERE (LA001 LIKE '4%' OR LA001 LIKE '5%')
+                                        AND LA009  IN ( '21002')
+                                        GROUP BY LA001, MB002
+                                        HAVING SUM(LA005 * LA011) > 0
+
+                                        UNION ALL
+
+                                        SELECT TB010 AS MB001, MB002 AS MB002
+                                        FROM [TK].dbo.POSTB
+                                        INNER JOIN [TK].dbo.INVMB ON TB010 = MB001
+                                        WHERE (TB010 LIKE '4%' OR TB010 LIKE '5%')
+                                        AND TB002   IN ( '106702')
+                                        AND TB001 = '{0}'
+                                        GROUP BY TB010, MB002
+                                        HAVING SUM(TB019) > 0
+
+	                                    UNION ALL
+
+	                                    SELECT LA001 AS MB001, MB002 AS MB002
+                                        FROM [TK].dbo.INVLA
+                                        INNER JOIN [TK].dbo.INVMB ON LA001 = MB001
+                                        WHERE (LA001 LIKE '4%' OR LA001 LIKE '5%')
+                                        AND LA009  IN ( '21002')
+	                                    AND LA005 IN (1)
+                                        AND LA004='{0}'
+                                        GROUP BY LA001, MB002
+                                        HAVING SUM(LA005 * LA011) > 0
+
+                                    ) AS TEMP
+                                    WHERE MB001 NOT LIKE '501%'
+                                    ORDER BY MB001, MB002
+
+                                    --更新庫存量
+                                    UPDATE [TKMK].[dbo].[TBDAILYPOSTB]
+                                    SET [NOWNUMS]=TEMP.NUMS
+                                    FROM 
+                                    (
+	                                    SELECT LA001,MB002,SUM(LA005*LA011) AS NUMS
+	                                    FROM [TK].dbo.INVLA,[TK].dbo.INVMB
+	                                    WHERE LA001=MB001
+	                                    AND (LA001 LIKE '4%' OR LA005 LIKE '5%')
+	                                    AND LA009 IN ('21002')
+	                                    GROUP BY  LA001,MB002
+                                    HAVING SUM(LA005*LA011)>0
+                                    ) AS TEMP
+                                    WHERE TEMP.LA001=[TBDAILYPOSTB].MB001
+                                    AND [TBDAILYPOSTB].[SDATES]='{0}'
+
+                                    --更新銷售量
+                                    UPDATE [TKMK].[dbo].[TBDAILYPOSTB]
+                                    SET [SALENUMS]=TEMP.TB019
+                                    FROM 
+                                    (
+	                                    SELECT TB010,MB002,SUM(TB019) TB019
+	                                    FROM [TK].dbo.POSTB,[TK].dbo.INVMB
+	                                    WHERE TB010=MB001
+	                                    AND (TB010 LIKE '4%' OR TB010 LIKE '5%')
+	                                    AND  TB002 IN ('106702')
+	                                    AND TB001='{0}'
+	                                    GROUP BY TB010,MB002
+	                                    HAVING SUM(TB019)>0
+                                    ) AS TEMP
+                                    WHERE TEMP.TB010=[TBDAILYPOSTB].MB001
+                                    AND [TBDAILYPOSTB].[SDATES]='{0}'
+
+                                    --更新進貨量
+                                    UPDATE [TKMK].[dbo].[TBDAILYPOSTB]
+                                    SET INNUMS=TEMP.NUMS
+                                    FROM 
+                                    (
+	                                    SELECT LA001, MB002,SUM(LA005*LA011) AS NUMS
+                                        FROM [TK].dbo.INVLA
+                                        INNER JOIN [TK].dbo.INVMB ON LA001 = MB001
+                                        WHERE (LA001 LIKE '4%' OR LA001 LIKE '5%')
+                                        AND LA009  IN ( '21002')
+	                                    AND LA005 IN (1)
+                                        AND LA004='{0}'
+                                        GROUP BY LA001, MB002
+                                        HAVING SUM(LA005 * LA011) > 0
+                                    ) AS TEMP
+                                    WHERE TEMP.LA001=[TBDAILYPOSTB].MB001
+                                    AND [TBDAILYPOSTB].[SDATES]='{0}'
+                                    "
+                                    , SDATES
+                                    );
+
+                sbSql.AppendFormat(@" ");
+
+                cmd.Connection = sqlConn;
+                cmd.CommandTimeout = 60;
+                cmd.CommandText = sbSql.ToString();
+                cmd.Transaction = tran;
+                result = cmd.ExecuteNonQuery();
+
+                if (result == 0)
+                {
+                    tran.Rollback();    //交易取消
+                }
+                else
+                {
+                    tran.Commit();      //執行交易  
+
+
+                }
+            }
+            catch
+            {
+
+            }
+
+            finally
+            {
+                sqlConn.Close();
+            }
+
+        }
         #endregion
 
         #region BUTTON
         private void button1_Click(object sender, EventArgs e)
         {
-            SEARCHGROUPSALES(dateTimePicker1.Value.ToString("yyyyMMdd"));
+            string SDATES = dateTimePicker1.Value.ToString("yyyyMMdd");
+            ADD_TBDAILYPOSTB(SDATES);
+            SEARCHGROUPSALES(SDATES);
         }
 
         private void button2_Click(object sender, EventArgs e)
